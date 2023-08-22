7a869fe23ee963c2f6f55d4323b86086b1328fa3
choi-sunbin <csn1434@naver.com>
2023-08-22 22:56:24 +0900

    MBP TEST

--- webtoon2/src/App.js
+++ webtoon2/src/App.js
@@ -7,10 +7,14 @@ function App() {
   const audioRef = useRef(null);
   const [currentAudioSrc, setCurrentAudioSrc] = useState('');
   const [currentIndex, setCurrentIndex] = useState(-1);
-  
+  const [wheelDownChk, setWheelDownChk] = useState(0);
   window.addEventListener("mousewheel", e => {
     const scrollY = window.scrollY;
         
+    if (lastScrollY < scrollY)
+      setWheelDownChk(1);
+    else
+      setWheelDownChk(0);
     // 현재의 스크롤 값을 저장
     setlastScrollY(scrollY);
   });
@@ -31,36 +35,45 @@ function App() {
     // IntersectionObserver 등록
     const io = new IntersectionObserver(entries => {
       entries.forEach(entry => {
-        // 관찰 대상이 viewport 안에 들어온 경우 'active' 클래스 추가
-        if (entry.intersectionRatio >= 0.7) {
-          let imgIndex = entry.target.src.split('/')[5].split('.')[0];
-          let audioSrc = require(`./audio/${imgIndex}.wav`);
-          if (currentIndex !== imgIndex && currentIndex !== -1)
-          {
-            setCurrentIndex(imgIndex);
-            console.log(currentIndex + " " + imgIndex)
-            audioRef.current.pause();
-            setCurrentIndex(imgIndex);
+        try{
+          // 관찰 대상이 viewport 안에 들어온 경우 'active' 클래스 추가
+          if (entry.intersectionRatio >= 0.4) {
+            let imgIndex = entry.target.src.split('/')[5].split('.')[0];
+            let audioSrc = require(`./audio/${imgIndex}.wav`);
+            if (
+              (currentIndex !== imgIndex && currentIndex !== -1) && 
+              ((wheelDownChk === 1 && imgIndex > currentIndex) || (wheelDownChk === 0 && imgIndex < currentIndex))) 
+            {
+              console.log(currentIndex + " " + imgIndex)
+              audioRef.current.pause();
+              setCurrentIndex(imgIndex);
 
-            var AudioContext;
-            var audioContext;
-              
-            navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
-                AudioContext = window.AudioContext || window.webkitAudioContext;
-                audioContext = new AudioContext();
-            }).catch(e => {
-                console.error(`Audio permissions denied: ${e}`);
-            }).then(()=>{
-              const audio = new Audio(audioSrc);
-              audio.play();
-              audioRef.current = audio; // 현재 오디오 업데이트
-              setCurrentAudioSrc(audioSrc); // 현재 오디오 소스 업데이트            
-            })
-          }    
-        }
-        // 그 외의 경우 'active' 클래스 제거
-        else {
-          entry.target.classList.remove('active');
+              var AudioContext;
+              var audioContext;
+                
+              navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
+                  AudioContext = window.AudioContext || window.webkitAudioContext;
+                  audioContext = new AudioContext();
+              }).catch(e => {
+                  console.error(`Audio permissions denied: ${e}`);
+              }).then(()=>{
+                const audio = new Audio(audioSrc);
+                var playPromise = audio.play();
+                
+                if (playPromise !== undefined) { 
+                  playPromise.then((_) => {}).catch((error) => {}); 
+                }
+                audioRef.current = audio; // 현재 오디오 업데이트
+                setCurrentAudioSrc(audioSrc); // 현재 오디오 소스 업데이트            
+              })
+            }    
+          }
+          // 그 외의 경우 'active' 클래스 제거
+          else {
+            entry.target.classList.remove('active');
+          }
+        } catch (err){
+          console.log(err);
         }
       })
     })
@@ -74,7 +87,8 @@ function App() {
   }, [lastScrollY]);
 
 
-  const imgList = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  const imgList = Array.from({ length: 21 }, (_, i) => i + 1);
   return (
     <div className="App">
       <header className="App-header">
@@ -94,7 +108,7 @@ function App() {
               key={i}
             >
               <img
-                src={require(`./img/${i}.jpg`)}
+                src={require(`./imageFinal/${i}.jpg`)}
                 className="webImg"
                 style={{
                   backgroundColor: "red",
