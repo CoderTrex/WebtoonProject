57953588cdd01aaf2c966fb6bfa863b57f8e6920
choi-sunbin <csn1434@naver.com>
2023-08-31 00:30:39 +0900

    appJS FIX

--- webtoon2/src/App.js
+++ webtoon2/src/App.js
@@ -1,100 +1,106 @@
-
 import "./App.css";
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from "react";
 
 function App() {
   const [lastScrollY, setlastScrollY] = useState(0);
   const audioRef = useRef(null);
-  const [currentAudioSrc, setCurrentAudioSrc] = useState('');
+  const [currentAudioSrc, setCurrentAudioSrc] = useState("");
   const [currentIndex, setCurrentIndex] = useState(-1);
   const [wheelDownChk, setWheelDownChk] = useState(0);
-  window.addEventListener("mousewheel", e => {
+
+  window.addEventListener("mousewheel", (e) => {
     const scrollY = window.scrollY;
-        
-    if (lastScrollY < scrollY)
-      setWheelDownChk(1);
-    else
-      setWheelDownChk(0);
+    console.log(scrollY);
+    if (lastScrollY < scrollY) setWheelDownChk(1);
+    else setWheelDownChk(0);
     // 현재의 스크롤 값을 저장
     setlastScrollY(scrollY);
   });
 
-  useEffect(()=> {
+  useEffect(() => {
     var AudioContext;
     var audioContext;
-      
-    navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
+
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(() => {
         AudioContext = window.AudioContext || window.webkitAudioContext;
         audioContext = new AudioContext();
-    }).catch(e => {
+      })
+      .catch((e) => {
         console.error(`Audio permissions denied: ${e}`);
-    })
-  })
+      });
+  });
 
   useEffect(() => {
     // IntersectionObserver 등록
-    const io = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        try{
+    const io = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        try {
           // 관찰 대상이 viewport 안에 들어온 경우 'active' 클래스 추가
+          // console.log(entry.intersectionRatio);
           if (entry.intersectionRatio >= 0.4) {
-            let imgIndex = entry.target.src.split('/')[5].split('.')[0];
+            let imgIndex = entry.target.src.split("/")[5].split(".")[0];
+            imgIndex = Number(imgIndex);
             let audioSrc = require(`./audio/${imgIndex}.wav`);
+            console.log(typeof imgIndex);
+
             if (
-              (currentIndex !== imgIndex && currentIndex !== -1) && 
-              ((wheelDownChk === 1 && imgIndex > currentIndex) || (wheelDownChk === 0 && imgIndex < currentIndex))) 
-            {
-              console.log(currentIndex + " " + imgIndex)
+              currentIndex !== -1 &&
+              ((wheelDownChk === 1 && imgIndex > currentIndex) ||
+                (wheelDownChk === 0 && imgIndex < currentIndex))
+            ) {
               audioRef.current.pause();
               setCurrentIndex(imgIndex);
 
               var AudioContext;
               var audioContext;
-                
-              navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
-                  AudioContext = window.AudioContext || window.webkitAudioContext;
+
+              navigator.mediaDevices
+                .getUserMedia({ audio: true })
+                .then(() => {
+                  AudioContext =
+                    window.AudioContext || window.webkitAudioContext;
                   audioContext = new AudioContext();
-              }).catch(e => {
+                })
+                .catch((e) => {
                   console.error(`Audio permissions denied: ${e}`);
-              }).then(()=>{
-                const audio = new Audio(audioSrc);
-                var playPromise = audio.play();
-                
-                if (playPromise !== undefined) { 
-                  playPromise.then((_) => {}).catch((error) => {}); 
-                }
-                audioRef.current = audio; // 현재 오디오 업데이트
-                setCurrentAudioSrc(audioSrc); // 현재 오디오 소스 업데이트            
-              })
-            }    
+                })
+                .then(() => {
+                  const audio = new Audio(audioSrc);
+                  var playPromise = audio.play();
+
+                  if (playPromise !== undefined) {
+                    playPromise.then((_) => {}).catch((error) => {});
+                  }
+                  audioRef.current = audio; // 현재 오디오 업데이트
+                  setCurrentAudioSrc(audioSrc); // 현재 오디오 소스 업데이트
+                });
+            }
           }
           // 그 외의 경우 'active' 클래스 제거
           else {
-            entry.target.classList.remove('active');
+            entry.target.classList.remove("active");
           }
-        } catch (err){
+        } catch (err) {
           console.log(err);
         }
-      })
-    })
-    if (lastScrollY === 0)
-      setCurrentIndex(0);
-    // 관찰할 대상을 선언하고, 해당 속성을 관찰  
-    const boxList = document.querySelectorAll('.webImg');
+      });
+    });
+    if (lastScrollY === 0) setCurrentIndex(0);
+    // 관찰할 대상을 선언하고, 해당 속성을 관찰
+    const boxList = document.querySelectorAll(".webImg");
     boxList.forEach((el) => {
       io.observe(el);
-    })
+    });
   }, [lastScrollY]);
-
-
 
   const imgList = Array.from({ length: 21 }, (_, i) => i + 1);
   return (
     <div className="App">
       <header className="App-header">
-        <audio ref={audioRef} >
-         <source src={currentAudioSrc} type="audio/wav" />
-       
+        <audio ref={audioRef}>
+          <source src={currentAudioSrc} type="audio/wav" />
         </audio>
         <table style={{ borderCollapse: "collapse" }}>
           {imgList.map((i) => (
